e9be4ed6c1bca5917e605a0723bf527072686e53
gaoyangbi <992219727@qq.com>
2024-08-08 18:45:03 +0800

    add

---
+++ jadeR_Fortran/x64/Debug/jader__genmod.f90
@@ -0,0 +1,13 @@
+        !COMPILER-GENERATED INTERFACE MODULE: Thu Aug  8 18:42:03 2024
+        ! This source file is for reference only and may not completely
+        ! represent the generated interface used by the compiler.
+        MODULE JADER__genmod
+          INTERFACE 
+            SUBROUTINE JADER(X,M,X_SIZE1,X_SIZE2)
+              INTEGER(KIND=4) :: X_SIZE2
+              INTEGER(KIND=4) :: X_SIZE1
+              REAL(KIND=8) :: X(X_SIZE1,X_SIZE2)
+              INTEGER(KIND=4) :: M
+            END SUBROUTINE JADER
+          END INTERFACE 
+        END MODULE JADER__genmod
